0b4edc4b8ec1079b71a91243cc8973f7d0d82486
Ye Yuan <khrylx@gmail.com>
2019-10-23 13:59:47 -0400

    added dls LPIPS.

--- compute_dists_dls.py
+++ compute_dists_dls.py
@@ -27,14 +27,14 @@ for i in range(opt.ninput):
         img_name = 'input_%03d_random_sample%02d.png' % (i, j + 1)
         img_path = os.path.join(results_dir, img_name)
         # print(img_path)
-        img = util.im2tensor(util.load_image(img_path)).to(device)
+        img = util.im2tensor(util.load_image(img_path))
         imgs.append(img)
 
     LPIPS = 0.0
     n_pairs = 0
     for p in range(1, len(imgs)):
         for q in range(p):
-            LPIPS += model.forward(imgs[q], imgs[p])
+            LPIPS += model.forward(imgs[q].to(device), imgs[p].to(device))
             n_pairs += 1
     LPIPS /= n_pairs
     t_LPIPS += LPIPS
